85e9f4e571cf84c485b86573f2c81b8edf5ddd93
Maguiii <magaliarebull@gmail.com>
2023-07-14 09:41:46 -0300

    inicio de repositorio

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include<Arduino.h>
+
+int Blue_LED_Pin = 3; // PWM Pin for Blue LED
+
+int Blue_value=0;
+
+char BluetoothData; // the data received from bluetooth serial link
+
+void setup() {
+  
+
+  pinMode(Blue_LED_Pin, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  
+  if (Serial.available()){
+    BluetoothData=Serial.read(); //Get next character from bluetooth
+ 
+    if(BluetoothData=='B') Blue_value=Serial.parseInt(); //Read Blue Value
+  }
+  
+
+  analogWrite(Blue_LED_Pin, Blue_value);
+  
+  delay(10);
+  
+}
